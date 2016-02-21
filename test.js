aad87623dfa6b05fdf7a4c210ccf4c132e19d627
Franklin van de Meent <fr@nkl.in>
2016-02-21 04:12:25 +0100

    Test: more useful test runner

--- test.js
+++ test.js
@@ -5,11 +5,17 @@ Feedback:       https://github.com/fvdm/nodejs-cloudsight/issues
 License:        Unlicense (public domain)
 */
 
-var app = require ('./');
-var cloudsight;
+var path = require ('path');
+var dir = path.dirname (module.filename);
+
+var pkg = require (path.join (dir, 'package.json'));
+var app = require (path.join (dir));
+
 var errors = 0;
+var warnings = 0;
 var queue = [];
 var next = 0;
+var cloudsight;
 
 
 // Setup
@@ -27,73 +33,203 @@ if (!config.apikey) {
 cloudsight = app (config);
 
 
+/**
+ * ANSI colorize a string
+ *
+ * @param color {String} - The color to add
+ * @param str {String} - The string to alter
+ * @returns {String}
+ */
+
+function colorStr (color, str) {
+  var colors = {
+    red: '\u001b[31m',
+    green: '\u001b[32m',
+    yellow: '\u001b[33m',
+    blue: '\u001b[34m',
+    magenta: '\u001b[35m',
+    cyan: '\u001b[36m',
+    gray: '\u001b[37m',
+    bold: '\u001b[1m',
+    plain: '\u001b[0m'
+  };
+
+  return colors [color] + str + colors.plain;
+}
+
+
+/**
+ * console.log with style
+ *
+ * @param [type] {String=plain} - Formatting style
+ * @param str {String} - The string to alter
+ * @returns {void}
+ */
+
+function log (type, str) {
+  var types = {
+    fail: ['red', 'FAIL'],
+    good: ['green', 'good'],
+    warn: ['yellow', 'warn'],
+    info: ['cyan', 'info']
+  };
+
+  if (!str) {
+    str = type;
+    type = 'plain';
+  }
+
+  switch (type) {
+    case 'error': console.log (colorStr ('red', colorStr ('bold', 'ERROR   ')) + str + '\n'); break;
+    case 'note': console.log (colorStr ('bold', str)); break;
+    case 'plain': console.log (str); break;
+    default:
+      console.log (colorStr (types[type][0], types[type][1]) + '    ' + str);
+      break;
+  }
+}
+
+
+/**
+ * Detect and wrap string type
+ *
+ * @param str {String} - The string
+ * @returns {String}
+ */
+
+function typeStr (str) {
+  if (typeof str === 'string') {
+    str = '"' + str + '"';
+  } else if (str instanceof Object) {
+    str = 'Object';
+  } else if (str instanceof Array) {
+    str = 'Array';
+  } else if (str instanceof Error) {
+    str = 'Error';
+  }
+
+  return colorStr ('magenta', str);
+}
+
 // handle exits
-process.on ('exit', function () {
-  if (errors === 0) {
-    console.log ('\n\u001b[1mDONE, no errors.\u001b[0m\n');
-    process.exit (0);
-  } else {
-    console.log ('\n\u001b[1mFAIL, ' + errors + ' error' + (errors > 1 ? 's' : '') + ' occurred!\u001b[0m\n');
+process.on ('exit', function processExit () {
+  console.log ();
+  log ('info', errors + ' errors');
+  log ('info', warnings + ' warnings');
+  console.log ();
+
+  if (errors) {
     process.exit (1);
+  } else {
+    process.exit (0);
   }
 });
 
 // prevent errors from killing the process
-process.on ('uncaughtException', function (err) {
+process.on ('uncaughtException', function uncaughtException (err) {
+  console.log (err);
   console.log ();
-  console.error (err.stack);
+  console.log (err.stack);
   console.log ();
   errors++;
 });
 
-// Queue to prevent flooding
+
+/**
+ * Queue to prevent flooding
+ *
+ * @returns {void}
+ */
+
 function doNext () {
   next++;
-  if (queue[next]) {
-    queue[next] ();
+  if (queue [next]) {
+    console.log ();
+    queue [next] ();
   }
 }
 
-// doTest (passErr, 'methods', [
-//   ['feeds', typeof feeds === 'object']
-// ])
+
+/**
+ * doTest checks for error
+ * else runs specified tests
+ *
+ * @param {Error} err
+ * @param {String} label
+ * @param {Array} tests
+ * @returns {void}
+ *
+ * doTest(err, 'label text', [
+ *   ['fail', 'feeds', typeof feeds, 'object'],
+ *   ['warn', 'music', music instanceof Array, true],
+ *   ['info', 'tracks', music.length]
+ * ]);
+ */
+
 function doTest (err, label, tests) {
-  var testErrors = [];
+  var level = 'good';
+  var test;
+  var i;
 
   if (err instanceof Error) {
-    console.error (label + ': \u001b[1m\u001b[31mERROR\u001b[0m\n');
+    log ('error', label);
     console.dir (err, { depth: null, colors: true });
     console.log ();
-    console.error (err.stack);
+    console.log (err.stack);
     console.log ();
     errors++;
-  } else {
-    tests.forEach (function (test) {
-      if (test[1] !== true) {
-        testErrors.push (test[0]);
-        errors++;
-      }
-    });
 
-    if (testErrors.length === 0) {
-      console.log (label + ': \u001b[1m\u001b[32mok\u001b[0m');
-    } else {
-      console.error (label + ': \u001b[1m\u001b[31mfailed\u001b[0m  (' + testErrors.join (', ') + ')');
+    doNext ();
+    return;
+  }
+
+  log ('note', colorStr ('blue', '(' + (next + 1) + '/' + queue.length + ') ') + label);
+
+  for (i = 0; i < tests.length; i++) {
+    test = {
+      level: tests [i] [0],
+      label: tests [i] [1],
+      result: tests [i] [2],
+      expect: tests [i] [3]
+    };
+
+    if (test.result === test.expect) {
+      log ('good', colorStr ('blue', test.label) + ': ' + typeStr (test.result) + ' is exactly ' + typeStr (test.expect));
+    }
+
+    if (test.level === 'fail' && test.result !== test.expect) {
+      errors++;
+      level = 'fail';
+      log ('fail', colorStr ('blue', test.label) + ': ' + typeStr (test.result) + ' is not ' + typeStr (test.expect));
+    }
+
+    if (test.level === 'warn' && test.result !== test.expect) {
+      warnings++;
+      level = level !== 'fail' && 'warn';
+      log ('warn', colorStr ('blue', test.label) + ': ' + typeStr (test.result) + ' is not ' + typeStr (test.expect));
+    }
+
+    if (test.level === 'info') {
+      log ('info', colorStr ('blue', test.label) + ': ' + typeStr (test.result));
     }
   }
 
   doNext ();
 }
 
-
-// TESTS
+// METHOD
 queue.push (function () {
   doTest (null, 'module', [
-    ['exports', typeof app === 'function'],
-    ['interface', cloudsight instanceof Object]
+    ['fail', 'exports', typeof app, 'function'],
+    ['interface', cloudsight instanceof Object, true]
   ]);
 });
 
 
 // Start the tests
-queue[0] ();
+log ('note', 'Running tests...\n');
+log ('note', 'Node.js:  ' + process.versions.node);
+log ('note', 'Module:   ' + pkg.version);
+console.log ();
+
+queue [0] ();
