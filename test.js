8428e7717e9e1995eed8790d8566729403525b68
Franklin van de Meent <fr@nkl.in>
2016-02-21 05:43:51 +0100

    Test: cleaner API endpoint log

--- test.js
+++ test.js
@@ -312,7 +312,7 @@ queue.push (function () {
 
 // Start the tests
 log ('note', 'Running tests...\n');
-log ('note', 'API endpoint:  ' + config.endpoint);
+log ('note', 'API endpoint:  ' + (config.apikey ? 'LIVE' : 'CUSTOM'));
 log ('note', 'Node.js:       ' + process.versions.node);
 log ('note', 'Module:        ' + pkg.version);
 console.log ();
